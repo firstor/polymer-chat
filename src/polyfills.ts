f5a9312b4f07b8694ca63071b704d04416e6b6d2
first87 <hotfly9@gmail.com>
2017-06-28 10:48:14 -0700

    Reorganize polyfill imports to reduce the chuck count

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,9 +14,6 @@
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
 
 import * as checkBrowserWithMinVersion from 'check-browser';
 let evergreenBrowser = checkBrowserWithMinVersion({
@@ -29,33 +26,10 @@ console.log('Import polyfills targeting an ' + (evergreenBrowser ? 'evergreen' :
 
 declare var System: any;
 
+/** Import basic polyfills required by Angular itself */
+import './polyfills.basic';
+
+/** Import optional polyfills to target browsers */
 if (!evergreenBrowser) {
-    System.import('./polyfills.es6');
-}
-
-
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
-
-/** IE10, IE11 and all Firefox browsers require the following to support `@angular/animation`. **/
-/** ALREADY IMPORTED WITH POLYMER COMPONENTS */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
-
-/***************************************************************************************************
- * Zone JS is required by Angular itself.
- */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-if (!evergreenBrowser) {
-    System.import('./polyfills.misc');
+    System.import('./polyfills.target');
 }
