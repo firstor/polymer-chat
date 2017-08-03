7cb08820fa07c08123d669dbe2833986adab459c
firstor <firstor87@gmail.com>
2017-08-03 04:44:40 -0700

    Refactor message service with http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import {PolymerModule} from '@codebakery/origami';
 import {AppElementsModule, IronElementsModule, PaperElementsModule} from '@codebakery/origami/lib/collections';
 import {AppComponent} from './app.component';
@@ -8,6 +9,10 @@ import {MessageService} from './message/message.service';
 import {MessageItemComponent} from './message/item/message-item.component';
 import {MessageInputComponent} from './message/input/message-input.component';
 import {MessagePostTimePipe} from './message/message-post-time.pipe';
+
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AppDataService} from './app-data.service';
 
 @NgModule({
     declarations: [
@@ -19,6 +24,8 @@ import {MessagePostTimePipe} from './message/message-post-time.pipe';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(AppDataService),
         PolymerModule.forRoot(),
         AppElementsModule,
         PaperElementsModule,
