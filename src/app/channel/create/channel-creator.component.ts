e814cf55a1d622146d2c01e4418a2cb993189522
firstor <firstor87@gmail.com>
2017-08-05 00:43:52 -0700

    Configure a form group for channel-create

--- src/app/channel/create/channel-creator.component.ts
+++ src/app/channel/create/channel-creator.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import {Channel} from '../channel.model';
 import {ChannelService} from '../channel.service';
 
@@ -7,9 +8,20 @@ import {ChannelService} from '../channel.service';
     templateUrl: './channel-creator.component.html',
     styleUrls: ['./channel-creator.component.css']
 })
-export class ChannelCreatorComponent {
+export class ChannelCreatorComponent implements OnInit {
+    public theForm: FormGroup;
 
-    constructor(private channelService: ChannelService) {
+    constructor(private channelService: ChannelService,
+                private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.theForm = this.formBuilder.group({
+            name: ['', Validators.required, Validators.maxLength(16)],
+            disp: ['', Validators.required, Validators.maxLength(32)],
+            desc: ['', Validators.maxLength(64)],
+            isPrivate: true
+        });
     }
 
     onSubmit(): void {
