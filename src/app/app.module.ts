46f57ede587f12487dae56607bcb64b6f1a7f90d
firstor <firstor87@gmail.com>
2017-08-05 10:30:21 -0700

    Reorganize app with multiple sub modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {PolymerModule} from '@codebakery/origami';
 import {AppElementsModule, IronElementsModule, PaperElementsModule} from '@codebakery/origami/lib/collections';
 
-import {SharedModule} from './shared/shared.module';
 import {AppComponent} from './app.component';
-import {ChatComponent} from './message/chat/chat.component';
-import {ChannelListComponent} from './channel/list/channel-list.component';
-import {ChannelItemComponent} from './channel/item/channel-item.component';
-import {ChannelCreatorComponent} from './channel/create/channel-creator.component';
-import {MessageItemComponent} from './message/item/message-item.component';
-import {MessageInputComponent} from './message/input/message-input.component';
-import {ChannelService} from './channel/channel.service';
-import {MessageService} from './message/message.service';
-import {MessagePostTimePipe} from './message/message-post-time.pipe';
+import {SharedModule} from './shared/shared.module';
+import {ChannelModule} from './channel/channel.module';
+import {MessageModule} from './message/message.module';
 
 // Imports for loading & configuring the in-memory web api
 import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
@@ -23,30 +16,20 @@ import {AppDataService} from './app-data.service';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ChatComponent,
-        ChannelListComponent,
-        ChannelItemComponent,
-        ChannelCreatorComponent,
-        MessageItemComponent,
-        MessageInputComponent,
-        MessagePostTimePipe
+        AppComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        ReactiveFormsModule,
         HttpModule,
         InMemoryWebApiModule.forRoot(AppDataService),
         PolymerModule.forRoot(),
         AppElementsModule,
-        PaperElementsModule,
         IronElementsModule,
-        SharedModule
-    ],
-    providers: [
-        ChannelService,
-        MessageService
+        PaperElementsModule,
+        SharedModule,
+        ChannelModule,
+        MessageModule
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     bootstrap: [AppComponent]
